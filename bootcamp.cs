cc6e72b8861a67813b58ce965df864286fd5ff97
haguen-nightingale <icksticks@gmail.com>
2019-05-22 02:51:32 -0300

    Add files via upload
    este é  o arquivo completo da classe bootcamp em c sharp

--- bootcamp.cs
+++ bootcamp.cs
@@ -1,84 +1,168 @@
-﻿using System;
-using System.Collections;
+class Bootcamp {
+	
 
-public class Bootcamp
-{
-    private Stack<int> internalCamp;
+ private int[,] internalCamp;
+ private int x, y;
 
-	public Bootcamp(int x = 1, int y = 1)
-	{
-        this.internalCamp;
-        if (!(x))
-        {
-            x = 1;
-        }
-        if (!(y))
-        {
-            y = 1;
-        }
-
-        this.buildCamp(0, 0, x, y);
+  Bootcamp( int x = 1, int y = 1) {
+    
+    if ( x <= 0 ) {
+      x = 1;
     }
 
-    private void buildCamp(int x, int y, int width, int height)
-    {
-        for (var x = r; r < width; r++)
-        {
-            Stack<int> row;
-            for(int c = y; c < height; c++)
-            {
-                row.Push(0);
-            }
-            this.internalCamp.push(row);
-        }
+    if ( y <= 0 ) {
+      y = 1;
     }
 
-    private int width {
-        set {
-            if(width == this.x)
-            {
-                return;
-            }
-            if(width > this.x)
-            {
-                this.buildCamp(this.x, 0, width, this.y);
-            }else if (width > 0)
-            {
-                do
-                {
-                    this.internalCamp.Pop();
-                } while (this.x > width);
-            }
-        }
-        get
-        {
-            return this.internalCamp.Pop();
-        }
+    x = (x <= 0 ) ? 1 : x;
+    y = (y <= 0 ) ? 1 : y;
+
+    this.x = x;
+    this.y = y;
+
+    this.buildCamp(0, 0, x, y);
+  }
+
+
+  private void buildCamp( int x, int y, int width, int height){
+    
+ 	this.internalCamp = new int[width,height];
+
+  	for(int row = x; row < width; row++){
+
+  		for(int column = y; column < height; column++){
+  			this.internalCamp[row,column] = 0;
+  		}
+  	}
+
+  }
+
+
+  public void setX( int width) {
+    
+    if( width < 0 ){
+    	width = 1;
     }
 
-    private int height
-    {
-        set
-        {
-            if(height == this.y)
-            {
-                return;
-            }
+	this.buildCamp(0, 0, width, this.y);
 
-            if (height > this.y)
-            {
-                const int oldHeight = this.y;
-                foreach{
-                    this.internalCamp(row =>
-                    {
-                        for(var c = oldHeight; c < height; c++)
-                        {
-                            row.
-                        }
-                    })
-                }
-                
-            }
-        }
+	this.x = width;
+  }
+
+  public int getX() {
+
+    return this.x;
+  
+  }
+
+
+
+  public void setY(int height) {
+
+    if( height < 0 ){
+    	height = 1;
     }
+
+	this.buildCamp(0, 0, this.x, height);
+
+	this.y = height;
+  }
+
+  public int getY(){
+
+    return this.y;
+  }
+
+
+  public int[,] getCamp(){
+
+    this._solve();
+    return this.internalCamp;
+  }
+
+  string display(int x, int y){
+
+    if (this.internalCamp[x,y] == 0) {
+      return " ";
+    }
+     else if (this.internalCamp[x,y] == -1) {
+      return "*";
+    }
+
+    return this.internalCamp[x,y].ToString();
+  }
+
+  public void changeBomb(int x, int y) {
+
+    if (this._isBomb(x, y)) {
+      this.clear(x, y);
+    } else {
+      this.setBomb(x, y);
+    }
+
+ }
+
+  void setBomb(int x, int y){
+
+    this.internalCamp[x,y] = -1;
+  }
+
+
+  void clear(int x, int y){
+
+    this.internalCamp[x,y] = 0;
+  }
+
+  private bool _existsInCamp(int x, int y) {
+      
+    return this.internalCamp[x,y] != null;
+    
+  }
+
+  private void _increment(int x, int y) {
+
+    if (this._existsInCamp(x, y) && this.internalCamp[x,y] > -1) {
+      this.internalCamp[x,y]++;
+    }
+
+  }
+
+  private bool _isBomb(int x, int y) {
+    return this.internalCamp[x,y] == -1;
+  }
+
+  private void _clear() {
+
+    for (int r = 0; r < this.x; r++) {
+      for (int c = 0; c < this.y; c++) {
+        if (!this._isBomb(r, c)) {
+          this.internalCamp[r,c] = 0;
+        }
+      }
+    }
+  }
+
+
+  private void _solve(){
+
+    this._clear();
+    
+    for (int r = 0; r < this.x; r++) {
+      for (int c = 0; c < this.y; c++) {
+        if (this._isBomb(r, c)) {
+          this._increment(r - 1, c - 1);
+          this._increment(r - 1, c);
+          this._increment(r - 1, c + 1);
+
+          this._increment(r, c - 1);
+          this._increment(r, c + 1);
+
+          this._increment(r + 1, c - 1);
+          this._increment(r + 1, c);
+          this._increment(r + 1, c + 1);
+        }
+      }
+    }
+  }
+
 }
